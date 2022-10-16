36032954f04a57b5bdc48df69b347e49daabb784
Rian Aldi <114709152+RianAldi@users.noreply.github.com>
2022-10-16 20:08:46 +0700

    Add files via upload

---
+++ pertemuan1.php
@@ -0,0 +1,21 @@
+<?php
+    $namaSaya = ucwords("rian aldi rivani");
+    $kelas=strtoupper("3KA23");
+    $alamat="Bekasi";
+    $golonganDarah="A";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Selamat Datang <?php echo $namaSaya;?></h1>
+    <p>Saya Kuliah Di kelas <?php echo $kelas;?></p>
+    <p>Saya Tinggal Di <?php echo $alamat?></p>
+    <p>Saya Memiliki Golongan Darah <?php echo $golonganDarah;?></p>
+</body>
+</html>
